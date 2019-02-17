316b0ec8f1fc37a78e48fb6610607100a0dc9310
Konstantin Nikitin <knikitin@gmail.com>
2019-02-17 23:34:16 +0300

    prepare data

--- main.js
+++ main.js
@@ -2,5 +2,14 @@ const fs = require('fs');
 
 const input = fs.readFileSync(process.argv[2], 'utf8').trim().split(/\n/);
 const [R, C, F, N, B, T] = input[0].split(/\s/).map(Number);
+const rides = input.slice(1).map(l => {
+  const [a, b, x, y, s, f] = l.split(/\s/).map(Number);
+  return {a, b, x, y, s, f};
+})
 
-console.log(input[0]);
+const v = []
+for (let i = 0; i < F; i++) {
+  v.push({x: 0, y: 0, r: [], t: 0})
+}
+// sort rides by start time
+rides.sort((a,b) => a.s - b.s);

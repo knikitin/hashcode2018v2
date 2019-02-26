1c94bcae8ae17fd4041ad62a3b8df8d451d238da
Konstantin Nikitin <knikitin@gmail.com>
2019-02-26 23:37:48 +0300

    upgrade the process to get a result

--- ca.js
+++ ca.js
@@ -4,6 +4,28 @@ const { execFile } = require('child_process');
 
 fs.readdir(dataFolder, (err, files) => {
   files.forEach(file => {
+    if (file.endsWith(".in")) {
+      console.log(file);
+      let ls = execFile('node', ['main', dataFolder + file], {shell: true}, 
+        (error, stdout, stderr) => {
+          if (error) {
+          throw error;
+          }
+          // console.log(stdout);
+          fs.writeFile(dataFolder + file + ".out", stdout, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file " + file + " was saved!");
+          });         
+        }
+      );
+    }
+  });
+});
+
+fs.readdir(dataFolder, (err, files) => {
+  files.forEach(file => {
     if (file.endsWith(".out")) {
       console.log(file);
       let ls = execFile('node', ['check', dataFolder + file], {shell: true}, 
@@ -12,7 +34,7 @@ fs.readdir(dataFolder, (err, files) => {
           throw error;
           }
           // console.log(stdout);
-          fs.writeFile(dataFolder + file + ".score", stdout, function(err) {
+          fs.appendFile(dataFolder + file + ".score", stdout, function(err) {
             if(err) {
                 return console.log(err);
             }
